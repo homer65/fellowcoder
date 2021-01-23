88d879819e25c40e34f4b36c9148cd01c602a93f
Christian <christian@myoggradio.org>
2021-01-23 19:58:50 +0100

    Neue Funktion aendern

--- Backend/python/controller.py
+++ Backend/python/controller.py
@@ -14,12 +14,38 @@ def controller(name):
     erg = '{"return":"ko","detail":"unbekannt"}'
     if name == "register.py": erg = register()
     elif name == "login.py": erg = login()
+    elif name == "aendern.py": erg = aendern()
     return erg 
 
 def get_uid():
     id_token = request.headers["id_token"]
+    if id_token == None: return "myoggradio" 
     user = auth.verify_id_token(id_token)
     return user["uid"]
+
+def aendern():
+    pseudonym = get_uid()
+    if pseudonym == None: return '{"return":"ko","detail":"Kein Pseudonym vorgegeben"}'
+    firestore = Firestore()
+    if not firestore.has_Pseudonym(pseudonym): return '{"return":"ko","detail":"Pseudonym existiert nicht"}'
+    dict = firestore.get_Benutzer(pseudonym)
+    feld = request.values.get("feld")
+    if feld == None: return '{"return":"ko","detail":"feld nicht angegeben"}'
+    wert = request.values.get("wert")
+    ok = False
+    if feld == "beschreibungstext": ok = True
+    elif feld == "bildurl": ok = True
+    elif feld == "geburtsdatum": ok = True
+    elif feld == "land": ok = True
+    elif feld == "name": ok = True
+    elif feld == "vorname": ok = True
+    elif feld == "sprachen": ok = True
+    if ok:
+        dict[feld] = wert
+        firestore.set_Pseudonym(pseudonym,dict)
+        return '{"return":"ok"}'
+    return '{"return":"ko","detail":"feld existiert nicht"}'
+        
 
 def login():
     pseudonym = get_uid()
